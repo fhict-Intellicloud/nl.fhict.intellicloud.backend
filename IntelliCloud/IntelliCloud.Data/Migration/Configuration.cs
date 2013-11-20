d55def39405b8418901eb304f80051d3d07f7cdc
teugis <teunvangisbergen@gmail.com>
2013-11-20 21:13:29 +0100

    Added data model migration configuration.

---
+++ IntelliCloud/IntelliCloud.Data/Migration/Configuration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity.Migrations;
+using nl.fhict.IntelliCloud.Data.Context;
+
+namespace nl.fhict.IntelliCloud.Data.Migration
+{
+    /// <summary>
+    /// Configuration class for the <see cref="IntelliCloudContext"/> model.
+    /// </summary>
+    internal sealed class Configuration : DbMigrationsConfiguration<IntelliCloudContext>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Configuration"/> class.
+        /// </summary>
+        public Configuration()
+        {
+            this.AutomaticMigrationsEnabled = true;
+        }
+    }
+}
