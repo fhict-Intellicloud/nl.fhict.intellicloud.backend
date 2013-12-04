10cd687350454bd24dbd70a88435ff8dc114609d
Joris Vervuurt <info@joris-vervuurt.com>
2013-12-04 15:56:45 +0100

    Added AuthorizationHandler, updated authorization classes.

--- IntelliCloud/IntelliCloud.Business/Authorization/AuthorizationToken.cs
+++ IntelliCloud/IntelliCloud.Business/Authorization/AuthorizationToken.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
+﻿using System.Runtime.Serialization;
 
 namespace nl.fhict.IntelliCloud.Business.Authorization
 {
@@ -22,25 +19,5 @@ namespace nl.fhict.IntelliCloud.Business.Authorization
         /// </summary>
         [DataMember(Name = "access_token")]
         public string AccessToken { get; set; }
-
-        /// <summary>
-        /// Method for parsing the Base64 representation of the JSON object to an instance of AuthorizationToken
-        /// </summary>
-        /// <param name="authenticationToken">Base64 encoded string of the JSON object (value of the AuthorizationToken HTTP header).<param>
-        /// <returns>Instance of class AuthorizationToken.</returns>
-        public static AuthorizationToken Parse(string authorizationToken)
-        {
-            // Decode the Base64 representation of the JSON object
-            byte[] tokenBytes = Convert.FromBase64String(authorizationToken);
-
-            using (MemoryStream stream = new MemoryStream(tokenBytes))
-            {
-                // Initialize serializer (used for deserializing the JSON representation of the AuthorizationToken)
-                DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(AuthorizationToken));
-                AuthorizationToken parsedToken = (AuthorizationToken)jsonSerializer.ReadObject(stream);
-
-                return parsedToken;
-            }
-        }
     }
 }
