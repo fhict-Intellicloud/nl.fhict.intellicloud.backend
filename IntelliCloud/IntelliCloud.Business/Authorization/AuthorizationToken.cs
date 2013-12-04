767b9f1e9dfe358ba8cc1c92c630c4ffedb5ca68
Joris Vervuurt <info@joris-vervuurt.com>
2013-12-04 15:34:31 +0100

    Added classes used for user authorization.

---
+++ IntelliCloud/IntelliCloud.Business/Authorization/AuthorizationToken.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace nl.fhict.IntelliCloud.Business.Authorization
+{
+    /// <summary>
+    /// Class that represents the contents of an authorization token sent in the AuthorizationToken HTTP header.
+    /// </summary>
+    [DataContract]
+    public class AuthorizationToken
+    {
+        /// <summary>
+        /// Gets or sets the issuer of the Access Token.
+        /// </summary>
+        [DataMember(Name = "issuer")]
+        public string Issuer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Access Token for retrieving available user information from the issuer.
+        /// </summary>
+        [DataMember(Name = "access_token")]
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// Method for parsing the Base64 representation of the JSON object to an instance of AuthorizationToken
+        /// </summary>
+        /// <param name="authenticationToken">Base64 encoded string of the JSON object (value of the AuthorizationToken HTTP header).<param>
+        /// <returns>Instance of class AuthorizationToken.</returns>
+        public static AuthorizationToken Parse(string authorizationToken)
+        {
+            // Decode the Base64 representation of the JSON object
+            byte[] tokenBytes = Convert.FromBase64String(authorizationToken);
+
+            using (MemoryStream stream = new MemoryStream(tokenBytes))
+            {
+                // Initialize serializer (used for deserializing the JSON representation of the AuthorizationToken)
+                DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(AuthorizationToken));
+                AuthorizationToken parsedToken = (AuthorizationToken)jsonSerializer.ReadObject(stream);
+
+                return parsedToken;
+            }
+        }
+    }
+}
