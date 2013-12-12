401791d284406964bed77342d5f3a02919a35fe5
koekoek <sirojsneksuelk@hotmail.com>
2013-12-12 16:31:01 +0100

    IntelliCloud Facebook service
    Windows service that:
    - Checks if the IntelliCloud Facebook page contains new posts
    - Sends new posts to database
    - Keeps track of the time of the post that was last send to the database

---
+++ IntelliCloudFacebookService/IntelliCloudFacebookService/JsonFacebookQuestion.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliCloudFacebookService
+{
+    class JsonFacebookQuestion
+    {
+        String _source;
+        String _reference;
+        String _question;
+        String _title;
+        String _postid;
+
+        /// <summary>
+        /// Create new QuestionMailObject
+        /// </summary>
+        /// <param name="source">The source of the question, in this case "Mail"</param>
+        /// <param name="reference">The e-mailaddress of the sender</param>
+        /// <param name="question">The content of the e-mail</param>
+        /// <param name="title">The subject of the e-mail</param>
+        public JsonFacebookQuestion(String source, String reference, String question, String title)
+        {
+            this._source = source;
+            this._reference = reference;
+            this._question = question;
+            this._title = title;
+            //this._postid = postid;
+        }
+
+        public String source 
+        {
+            get
+            {
+                return _source;
+            }
+        }
+
+        public String reference
+        {
+            get
+            {
+                return _reference;
+            }
+        }
+
+        public String question
+        {
+            get
+            {
+                return _question;
+            }
+        }
+
+        public String title
+        {
+            get
+            {
+                return _title;
+            }
+        }
+
+        //public String postId
+        //{
+        //    get
+        //    {
+        //        return _postid;
+        //    }
+        //}
+    }
+}
