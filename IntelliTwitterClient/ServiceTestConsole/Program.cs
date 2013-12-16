83d81ec99048261bbea36a02d8ada1390660a5e1
daanvc <daniel536605@hotmail.com>
2013-12-16 11:18:51 +0100

    Twitter test client created

---
+++ IntelliTwitterClient/ServiceTestConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using IntelliTwitterClient.Business;
+using IntelliTwitterClient.Business.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventLog log = new EventLog();
+
+            log.Source = "C:/Users/Daniel van Cann/Documents/GitHub/nl.fhict.intellicloud.backend/IntelliTwitterClient/IntelliTwitterClient/bin/Debug/log";
+
+            TwitterManager manager = new TwitterManager(log);
+
+            manager.StartStreaming();
+
+            Console.ReadLine();
+        }
+    }
+}
