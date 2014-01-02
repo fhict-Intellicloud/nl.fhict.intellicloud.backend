5a7189274788d184b9e01819fc23dfc014972514
SimonBorger <simonborger@gmail.com>
2014-01-02 14:14:50 +0100

    Committed wordstore

---
+++ WordStore/WordStore.Business/Conversion/IConversion.cs
@@ -0,0 +1,47 @@
+﻿using IntelliCloud.WordStore.Common.Word;
+using IntelliCloud.WordStore.Data.Database.Model;
+using System.Collections.Generic;
+using WordObject = IntelliCloud.WordStore.Common.Word.Word;
+
+namespace IntelliCloud.WordStore.Business.Conversion
+{
+    /// <summary>
+    /// An interface for a conversion object providing all functionality to convert objects.
+    /// </summary>
+    public interface IConversion
+    {
+        #region Methods
+
+        /// <summary>
+        /// Converts an instance of <see cref="WordEntity"/> to a corresponding instance of <see cref="WordObject"/>.
+        /// </summary>
+        /// <param name="wordEntity">The instance of <see cref="WordEntity"/> that must be converted.</param>
+        /// <returns>The converted <see cref="WordEntity"/> as an instance of <see cref="WordObject"/>.</returns>
+        WordObject ToWord(WordEntity wordEntity);
+
+        /// <summary>
+        /// Converts a string representing a keyword and a collection of <see cref="WordObject"/> instances 
+        /// representing its matching words into an instance of <see cref="KeywordEntity"/>.
+        /// </summary>
+        /// <param name="keyword">The keyword.</param>
+        /// <param name="words">The collection of words that match the <paramref name="keyword"/>.</param>
+        /// <returns>The converted <see cref="WordEntity"/> as an instance of <see cref="WordObject"/>.</returns>
+        KeywordEntity ToKeywordEntity(string keyword, IEnumerable<WordObject> matchingWords);
+
+        /// <summary>
+        /// Converts a string representing a word type to its corresponding <see cref="WordType"/> value.
+        /// </summary>
+        /// <param name="wordType">The string representing a word type.</param>
+        /// <returns>The corresponding <see cref="WordType"/> value.</returns>
+        WordType ToWordType(string wordType);
+
+        /// <summary>
+        /// Converts a string representing a language into its corresponding value of <see cref="Language"/>.
+        /// </summary>
+        /// <param name="language">The string representing a language.</param>
+        /// <returns>The corresponding value of <see cref="Language"/>.</returns>
+        Language ToLanguage(string language);
+
+        #endregion Methods
+    }
+}
---
+++ WordStore/WordStore.Business/Word/IWordManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordObject = IntelliCloud.WordStore.Common.Word.Word;
+
+namespace IntelliCloud.WordStore.Business.Word
+{
+    /// <summary>
+    /// An interface for a business manager providing functionality to resolve a string into a list of actual words 
+    /// including its language and type.
+    /// </summary>
+    public interface IWordManager
+    {
+        /// <summary>
+        /// Resolves a string that represents a word into all occurrences of this word in both the dutch and english 
+        /// dictionary. For instance, the word 'pen' will resolve to:
+        /// <ul>
+        ///     <li>The dutch noun 'pen'</li>
+        ///     <li>The english noun 'pen'</li>
+        ///     <li>The english verb 'to pen'</li>
+        /// </ul>
+        /// </summary>
+        /// <param name="word">The word that must be resolved.</param>
+        /// <returns>The instances of <see cref="Word"/> associated with the given <paramref name="word"/>.</returns>
+        /// <remarks>Note that when a word could not be found in any of the available dictionaries, a collection with 
+        /// a single instance of <see cref="Word"/> will be returned of type <see cref="WordType.Unknown"/> and 
+        /// language <see cref="Language.Unknown"/>.</remarks>
+        IEnumerable<WordObject> ResolveWord(string word);
+    }
+}
