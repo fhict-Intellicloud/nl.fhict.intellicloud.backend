9d078d6bf7121f6a1f64b3a91f498bcacf1d3e35
daanvc <daniel536605@hotmail.com>
2014-01-08 11:20:02 +0100

    removed unnessesery usings

--- IntelliCloudFacebookService/IntelliCloudFacebookService/FacebookService.cs
+++ IntelliCloudFacebookService/IntelliCloudFacebookService/FacebookService.cs
@@ -1,20 +1,12 @@
 ﻿using Facebook;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
-using System.Data;
 using System.Diagnostics;
-using System.Dynamic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 using System.Timers;
-using System.Xml;
 
 namespace IntelliCloudFacebookService
 {
