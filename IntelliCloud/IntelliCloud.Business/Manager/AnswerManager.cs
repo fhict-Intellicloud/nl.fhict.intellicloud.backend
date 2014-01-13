f2a56f72389592e3beb3069d39ccfb706c49b12b
daanvc <daniel536605@hotmail.com>
2014-01-13 14:33:15 +0100

    implemented algorithm in the create answer method

--- IntelliCloud/IntelliCloud.Business/Manager/AnswerManager.cs
+++ IntelliCloud/IntelliCloud.Business/Manager/AnswerManager.cs
@@ -91,13 +91,16 @@ namespace nl.fhict.IntelliCloud.Business.Manager
 
                 answerEntity.User = user;
 
-                // TODO determine real language 
-                LanguageDefinitionEntity languageDefinition = ctx.LanguageDefinitions.SingleOrDefault(ld => ld.Name.Equals("English"));
+                var words = this.ResolveWords(answer);
+                var language = this.GetLanguage(words);
+                var keywords = this.RetrieveKeywords(words, language);
 
-                if (languageDefinition == null)
+                var languageDefinitionEntity = ctx.LanguageDefinitions.AsEnumerable().Single(x => x.Name == this.ToLanguageString(language));
+
+                if (languageDefinitionEntity == null)
                     throw new NotFoundException("No languageDefinition entity exists with the specified name.");
 
-                answerEntity.LanguageDefinition = languageDefinition;
+                answerEntity.LanguageDefinition = languageDefinitionEntity;
 
                 ctx.Answers.Add(answerEntity);
 
@@ -119,9 +122,20 @@ namespace nl.fhict.IntelliCloud.Business.Manager
                 question.FeedbackToken = token.ToString();
 
                 answerEntity.IsPrivate = question.IsPrivate;
+                
+                var keywordEntities = this.GetOrCreateKeywordEntities(ctx, keywords);
+                var answerKeys = keywordEntities.Select(keyword =>
+                    new AnswerKeyEntity
+                    {
+                        Answer = answerEntity,
+                        Keyword = keyword.Value,
+                        CreationTime = DateTime.UtcNow,
+                        Affinity = keyword.Key.Affinity,
+                    });
 
+                ctx.AnswerKeys.AddRange(answerKeys);
                 ctx.SaveChanges();
-
+                
                 this.SendAnswerFactory.LoadPlugin(question.Source.Source.SourceDefinition).SendAnswer(question, answerEntity);
             }
 
@@ -173,13 +187,13 @@ namespace nl.fhict.IntelliCloud.Business.Manager
                 Language lan = this.GetLanguage(resolvedWords);
 
                 // find keywords
-                List<Word> keywords = new List<Word>();
-                keywords.AddRange(this.FindMostLikelyKeywords(resolvedWords, lan));
+                List<Entities.Keyword> keywords = new List<Entities.Keyword>();
+                keywords.AddRange(this.RetrieveKeywords(resolvedWords, lan));
 
                 List<string> keywordStringval = new List<string>();
-                foreach (Word w in keywords)
+                foreach (Entities.Keyword w in keywords)
                 {
-                    keywordStringval.Add(w.Value);
+                    keywordStringval.Add(w.Word.Value);
                 }
 
                 // find answer with these keywords
@@ -301,12 +315,12 @@ namespace nl.fhict.IntelliCloud.Business.Manager
         /// </summary>
         /// <param name="question">A string representing a question that is to be decomposed.</param>
         /// <returns> A list with each word in the sentence is returned. </returns>
-        internal IList<string> ConvertQuestion(String question)
+        internal IList<string> ConvertAnswer(String answer)
         {
             //Regex that matches every single special character exluding the -
             Regex regex = new Regex("[\\\\+=`~!@#$%^&*()_\\\\\\\\[\\]{}:\\\";\\?<>/.,|]");
 
-            string cleanQuestion = regex.Replace(question.ToLowerInvariant(), "");
+            string cleanQuestion = regex.Replace(answer.ToLowerInvariant(), "");
 
             return cleanQuestion.Split(' ')
                 .Where(x => x != "-" && x != "'")
@@ -319,12 +333,14 @@ namespace nl.fhict.IntelliCloud.Business.Manager
         /// </summary>
         /// <param name="question">A given question for which all words are to be resolved to their type.</param>
         /// <returns>A list containing the resolved words that were contained in the question</returns>
-        internal IList<Word> ResolveWords(String question)
+        internal IList<Word> ResolveWords(String answer)
         {
-            IWordService service = new WordServiceClient();
-            return ConvertQuestion(question)
-                .SelectMany(x => service.ResolveWord(x))
-                .ToList();
+            using (WordStoreContext context = new WordStoreContext())
+            {
+                return ConvertAnswer(answer)
+                    .SelectMany(x => context.ResolveWord(x))
+                    .ToList();
+            }
         }
 
         /// <summary>
@@ -333,13 +349,19 @@ namespace nl.fhict.IntelliCloud.Business.Manager
         /// <param name="question">A question from which one needs the keywords.</param>
         /// <param name="language">The language one needs the found keywords of. </param>
         /// <returns>Returns a List containing the most likely keywords from a given question.</returns>
-        internal IList<Word> FindMostLikelyKeywords(IList<Word> words, Language language)
+        internal IList<Entities.Keyword> RetrieveKeywords(IList<Word> words, Language language)
         {
             return words
-                .Where(x =>
-                    (x.Type == WordType.Noun || x.Type == WordType.Verb || x.Type == WordType.Pronoun)
-                    && x.Language == language)
-                .ToList();
+                .Where(x => x.Language == language || x.Language == Language.Unknown && x.Type != WordType.Article)
+                .GroupBy(x => x.Value)
+                .Select(x =>
+                    new Entities.Keyword(
+                        word: x.First(),
+                        affinity:
+                            ((x.First().Type == WordType.Noun || x.First().Type == WordType.Verb || x.First().Type == WordType.Pronoun))
+                            ? x.Count() * 10 :
+                            x.Count()
+                    )).ToList();
         }
 
         /// <summary>
@@ -355,6 +377,46 @@ namespace nl.fhict.IntelliCloud.Business.Manager
 
             return distinctLanguages.Single(x => x.Count == distinctLanguages.Max(y => y.Count)).Language;
         }
+
+        private string ToLanguageString(Language language)
+        {
+            switch (language)
+            {
+                case Language.Dutch:
+                    return "Dutch";
+                case Language.English:
+                    return "English";
+                case Language.Unknown:
+                    return "Unknown";
+                default:
+                    throw new ArgumentException("Unknown language");
+            }
+        }
+
+        private IDictionary<Entities.Keyword, KeywordEntity> GetOrCreateKeywordEntities(IntelliCloudContext context, IList<Entities.Keyword> keywords)
+        {
+            IDictionary<Entities.Keyword, KeywordEntity> entities = new Dictionary<Entities.Keyword, KeywordEntity>();
+            keywords.ToList().ForEach(
+                (keyword) =>
+                {
+
+                    var entity = context.Keywords
+                        .SingleOrDefault(x => x.Name == keyword.Word.Value);
+
+                    entities.Add(keyword, entity != null
+                        ? entity
+                        : new KeywordEntity
+                        {
+                            CreationTime = DateTime.UtcNow,
+                            Name = keyword.Word.Value,
+                        });
+                });
+
+            context.SaveChanges();
+
+            return entities;
+        }
+
         #endregion
     }
 }
